c3b6bff36e53a48c125f80b4b08888179b0a5d5a
Zachary Taylor <ztay90@gmail.com>
2020-03-17 20:39:36 -0400

    finished up project

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person(name) {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length !== 10) {
+    this.stomach.push(someFood)
+  }
+}
+Person.prototype.poop = function () {
+  this.stomach = [];
+}
+Person.prototype.toString = function () {
+return `${this.name}, ${this.age}`
+}
+
+
 
 /*
   TASK 2
@@ -57,8 +73,14 @@ function Person(name) {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
 }
 
 /*
@@ -68,18 +90,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.favoriteToy = favoriteToy;
+  Person.call(this, name, age)
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. WINDOW/GLOBAL OBJECT BINDING - When in global scope, the value of "this" will be the window/ console Object.
+  2. IMPLICIT BINDING - When a preceding dot calls a function.
+  3. NEW BINDING - When using a constructor function, this refers to the specific object that is created and returned by the constructor.
+  4. EXPLICIT BINDGING - JavaScript's call or apply method, this is explicitly defined.
 */
 
 
